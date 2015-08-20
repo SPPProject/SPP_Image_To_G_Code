e87a60027aa5a33cef7ff38317f025344631c1dd
ailersic <andrew.ilersich@mail.utoronto.ca>
2015-08-20 14:16:15 -0400

    Following update to library syntax

--- main_gui.py
+++ main_gui.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter.filedialog import askopenfilename
 import tkinter as tk
-import im2glib
+import im2glib as im2g
 import turtle
 
 FONT = "Trebuchet"
@@ -16,18 +16,18 @@ class Application(tk.Frame):
         self.filename = ""
 
     def createWidgets(self):
-        self.imInput = Entry(self, width = 50, font = (FONT, 12))
+        self.imInput = Entry(self, width = 54, font = (FONT, 12))
         self.imInput.delete(0, END)
-        self.imInput.insert(0, "Enter the name of your image file!")
+        self.imInput.insert(0, "Enter the name of your image file")
         self.imInput.grid(row = 0, column = 0, padx = 0, pady = 10,\
                           columnspan = 2)
 
-        self.browseButton = tk.Button(self)
+        self.browseButton = tk.Button(self, width = 20, font = (FONT, 12))
         self.browseButton["text"] = "Browse"
         self.browseButton["command"] = self.browse
         self.browseButton.grid(row = 1, column = 0)
         
-        self.plotButton = tk.Button(self)
+        self.plotButton = tk.Button(self, width = 20, font = (FONT, 12))
         self.plotButton["text"] = "Show Path"
         self.plotButton["command"] = self.plot
         self.plotButton.grid(row = 1, column = 1)
@@ -37,20 +37,19 @@ class Application(tk.Frame):
                              pady = 10)
         
         self.turtleSheet = turtle.TurtleScreen(self.turtlePlot)
-        self.turtleSheet.setworldcoordinates(0, 0, im2glib.config.imdim,\
-                                   im2glib.config.imdim)
+        self.turtleSheet.setworldcoordinates(0, 0, im2g.IMDIM, im2g.IMDIM)
         
         self.router = turtle.RawTurtle(self.turtleSheet)
         self.router.shape("circle")
         self.router.setx(0)
         self.router.sety(0)
 
-        self.fileButton = tk.Button(self)
+        self.fileButton = tk.Button(self, width = 20, font = (FONT, 12))
         self.fileButton["text"] = "Send to Text File"
         self.fileButton["command"] = self.toText
         self.fileButton.grid(row = 3, column = 0, pady = 10)
 
-        self.serialButton = tk.Button(self)
+        self.serialButton = tk.Button(self, width = 20, font = (FONT, 12))
         self.serialButton["text"] = "Send to Serial Port"
         self.serialButton["command"] = self.toSerial
         self.serialButton.grid(row = 3, column = 1, pady = 10)        
@@ -80,23 +79,24 @@ class Application(tk.Frame):
     
     def plot(self):
         self.filename = self.imInput.get()
-        if self.paths == []: self.paths = im2glib.imToPaths(self.filename)
+        if self.paths == []: self.paths = im2g.imToPaths(self.filename)
         
         self.turtleDraw()
 
     def toText(self):
         self.filename = self.imInput.get()
-        if self.paths == []: self.paths = im2glib.imToPaths(self.filename)
+        if self.paths == []: self.paths = im2g.imToPaths(self.filename)
         
         outfile = self.filename.rsplit(".", 1)[0] + ".gcode"
-        im2glib.toTextFile(outfile, self.paths)
+        im2g.toTextFile(outfile, self.paths)
     
     def toSerial(self):
         self.filename = self.imInput.get()
-        if self.paths == []: self.paths = im2glib.imToPaths(self.filename)
+        if self.paths == []: self.paths = im2g.imToPaths(self.filename)
         
-        im2glib.toSerial(self.paths)
+        im2g.toSerial(self.paths)
 
 root = tk.Tk()
+root.wm_title("Image to G Code")
 app = Application(master=root)
 app.mainloop()
